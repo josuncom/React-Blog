c4965352d60d51261cebe09655e816fdb99bd446
username <euny_sky11@naver.com>
2021-08-08 19:37:38 +0900

    21.08.08

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -16,18 +17,25 @@ class App extends Component {
       selectedVideo : null
      };
 
-    YTSearch({key : API_KEY, term : 'nba'}, (videos) => {
+     this.videoSearch('nba');
+
+    
+  }
+
+  videoSearch(term){
+    YTSearch({key : API_KEY, term : term}, (videos) => {
       this.setState({
         videos : videos,
         selectedVideo : videos[0]});
     });
   }
 
-
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
     return (
     <div>
-      <SearchBar/>
+      <SearchBar onSearchTermChange={videoSearch}/>
       <VideoDetail video={ this.state.selectedVideo }/>
       <VideoList 
         onVideoSelect = { selectedVideo => this.setState({selectedVideo})}
--- src/search_bar.js
+++ src/search_bar.js
@@ -10,9 +10,16 @@ class SearchBar extends Component{
     render(){
         return (
         <div className="search-bar">
-            <input onChange={(e) => this.setState({ term : e.target.value})} />
+            <input 
+            value={this.state.term}
+            onChange={event => this.onInputChange(event.target.value)} />
         </div>
         );
+    }
+
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
