291fa54a3d5c764342dffe978a7630ad3c9b2d71
username <euny_sky11@naver.com>
2022-01-10 16:40:47 +0900

    Profile~ing

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,33 @@
 import React from "react";
 import '../components/Profile.css';
-import { Link } from 'react-router-dom';
+import ReactTypingEffect from 'react-typing-effect';
 
 export default function Profile() {
     return(
         <>
-            <div className="ProfileContainer">profile
-                <p>This is Profile Component</p>
-            </div>  
+            <div className="ProfileContainer">
+            <div className="ProfileTitle">Profile</div>
+                <div className="ProfileBox">
+                    <div className="ProfileImage">
+                        <img src={require('./images/조은.jpg').default}/>
+                    </div>
+                    <div className="ProfileContents">
+                        <ReactTypingEffect speed="100" typingDelay="1000" eraseSpeed="200" eraseDelay="10000" 
+                        text={["이 름  :  조 은"]}/><br/>
+                        <hr className="ProfileLine"/>
+                        <ReactTypingEffect speed="100" typingDelay="1000" eraseSpeed="200" eraseDelay="10000" 
+                        text={["이 름  :  조 은"]}/><br/>
+                        <hr className="ProfileLine"/>
+                        <ReactTypingEffect speed="100" typingDelay="1000" eraseSpeed="200" eraseDelay="10000" 
+                        text={["이 름  :  조 은"]}/><br/>
+                        <hr className="ProfileLine"/>
+                        <ReactTypingEffect speed="100" typingDelay="1000" eraseSpeed="200" eraseDelay="10000" 
+                        text={["이 름  :  조 은"]}/><br/><br/>
+                        <ReactTypingEffect speed="100" typingDelay="1000" eraseSpeed="200" eraseDelay="10000" 
+                        text={["이 름  :  조 은"]}/><br/><br/>
+                    </div>
+                        </div>    
+                    </div>
         </>
     )
 }
